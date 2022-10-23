1af40529f47a8369a166e82a24435015b7f80966
Fathony Luthfillah <fathony@smart-leaders.net>
2022-10-23 22:46:35 +0700

    rename (prepare to parolaFont

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { CatScratchEditorProvider } from './catScratchEditor';
+import { CatScratchEditorProvider } from './parolaSpriteEditor';
 
 export function activate(context: vscode.ExtensionContext) {
 	const openWithCommand = vscode.commands.registerCommand("parola.spriteEditor.openFile", () => {
